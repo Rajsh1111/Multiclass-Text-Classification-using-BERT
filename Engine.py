443753cec9a4547d83091088792f5d927ec72b0d
Rajeev Sharma <123724904+Rajsh1111@users.noreply.github.com>
2024-08-02 16:42:28 +0530

    Update Engine.py

--- Engine.py
+++ Engine.py
@@ -1,58 +1,48 @@
-import torch
-import config
-from Source.data import TextDataset
-from Source.utils import load_file, save_file
-from sklearn.model_selection import train_test_split
-from Source.model import BertClassifier, train, test
+# import required libraries
+import pandas as pd
+import ktrain
+from ktrain import text
+from datasets import list_datasets
+from datasets import load_dataset
+from ML_Pipeline import model
+from ML_Pipeline import utils
+from ML_Pipeline import feature_engineering
 
+import warnings
 
-def main():
+warnings.simplefilter(action='ignore')
 
-    print("Loading the files...")
-    tokens = load_file(config.tokens_path)
-    labels = load_file(config.labels_path)
-    labels = labels[:1000]
-    label_encoder = load_file(config.label_encoder_path)
-    num_classes = len(label_encoder.classes_)
+try:
+    # Load Dataset and show details:
+    print('##### Load Dataset and Show Details #####')
+    utils.load_and_display_dataset_details()
 
-    print("Splitting data into train, valid and test sets...")
-    X_train, X_test, y_train, y_test = train_test_split(tokens, labels,
-                                                        test_size=0.2)
-    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train,
-                                                          test_size=0.25)
+    # Load Train-Test data and convert to DataFrame Object for further operations:
+    print('##### Load Train-Test data and convert to DataFrame Object for further operations #####')
+    ag_news_train_df, ag_news_test_df, class_label_names = utils.load_and_convert_data_to_df()
 
-    print("Creating PyTorch datasets...")
-    train_dataset = TextDataset(X_train, y_train)
-    valid_dataset = TextDataset(X_valid, y_valid)
-    test_dataset = TextDataset(X_test, y_test)
+    # Data Preprocessing using K-Train:
+    print('##### Data Preprocessing using K-Train #####')
+    (X_train, y_train), (X_test, y_test), preprocessing_var = feature_engineering.perform_feature_engineering(
+        ag_news_train_df, ag_news_test_df, 512)
 
-    print("Creating data loaders...")
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16,
-                                               shuffle=True, drop_last=True)
-    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=16)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16)
+    # Create & Train BERT Model:
+    print('##### Create & Train BERT Model #####')
+    bert_learner = model.create_and_train_bert_model(X_train, y_train, X_test, y_test, preprocessing_var)
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("Creating model object...")
-    model = BertClassifier(config.dropout, num_classes)
-    model_path = config.model_path
+    # Check Model performance during training and validation:
+    print('##### Check Model performance during training and validation #####')
+    model.check_model_performance(bert_learner, class_label_names)
 
-    # Define loss function and optimizer
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+    # Saving Bert Model Fine-tuned on AG News Dataset:
+    print('##### Saving Bert Model Fine-tuned on AG News Dataset #####')
+    model.save_fine_tuned_bert_model(bert_learner, preprocessing_var)
 
-    # Move model to GPU if GPU is available
-    if torch.cuda.is_available():
-        model = model.cuda()
-        criterion = criterion.cuda()
+    # Load Fine-tuned Bert Model for further predictions:
+    print('##### Load Fine-tuned Bert Model for further predictions #####')
+    bert_predictor = model.load_model()
 
-    print("Training the model...")
-    train(train_loader, valid_loader, model, criterion, optimizer,
-          device, config.num_epochs, model_path)
-
-    print("Testing the model...")
-    test(test_loader, model, criterion, device)
-
-
-if __name__ == "__main__":
-    main()
+except Exception as e:
+    print(e)
+    print('!! Exception Details: !!\n', e.__class__)
+    print('Please debug for further details')
